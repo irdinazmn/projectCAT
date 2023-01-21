afffd19539ac68f50f27e04b5171c9d42464a96d
Pmaneemay <mayratdaa.pmanee@gmail.com>
2023-01-21 23:31:17 +0800

    add methods to retrieve data from table

--- sources/org/payroll/DatabaseManager.java
+++ sources/org/payroll/DatabaseManager.java
@@ -128,179 +128,389 @@ public class DatabaseManager {
 		}
 	}
 
-//	public void changePassword(String username, String newPassword) {
-//		try {
-//			curs.executeUpdate(
-//					"UPDATE login_ids SET password=\"" + newPassword + "\" WHERE username=\"" + username + "\""
-//				);
-//		} catch (SQLException e) {
-//			System.err.println(e.getMessage());
-//		}
-//	}
+	//check if employee already log in for the day
+	public Boolean VerifyClockin(String empID) {
+		try {
+			return curs.executeQuery(
+					"SELECT  clock_in_time FROM Attendance WHERE ( emp_id =\"" + empID + "\")"
+							+ "AND ( attendance_date = CURRENT_DATE ) AND ( clock_in_time is not null ) "
+			).next();
+
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+		return false;
+	}
+
+	// check if employee already clock out for the day
+	public Boolean VerifyClockOut(String empID) {
+		try {
+			return curs.executeQuery(
+					"SELECT clock_out_time FROM Attendance WHERE ( emp_id =\"" + empID + "\")"
+							+ "AND ( attendance_date = CURRENT_DATE ) AND ( clock_out_time is not null ) "
+			).next();
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		return false;
+	}
+
+	//insert today's date into attendance date , current time in clock_in_time
+	public void PunchIn(String empID) {
+		try {
+			curs.executeUpdate(
+					"INSERT INTO Attendance ( clock_in_id, emp_id , attendance_date , clock_in_time )" +
+							"VALUES( null ,\"" + empID + "\"," + "CURRENT_DATE , CURRENT_TIME )"
+			);
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+	}
+
+	//update the clock_out_time to current time
+	public void PunchOut(String empID) {
+		try {
+			curs.executeUpdate(
+					"UPDATE Attendance " +
+							"SET clock_out_time = CURRENT_TIME " +
+							"WHERE  emp_id = \"" + empID + "\"" +
+							"AND attendance_date = CURRENT_DATE"
+
+			);
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+	}
+
+	//Insert salary for employee
+	public void InsertEmpSalary(String Date, String Emp_id, String Emp_Fullname, double totalPay, double totalHour) {
+		try {
+			curs.executeUpdate("INSERT INTO emp_salary ( date_salary , emp_id , emp_name , salary_per_day , total_time )" +
+					" VALUES (" + "\"" + Date + "\"," + "\"" + Emp_id + "\"," + "\"" + Emp_Fullname + "\"," + Double.toString(totalPay)
+					+ "," + Double.toString(totalHour) + ")");
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	// Retrieve the total hours for employee for each day from table , get the hourly rate and OT rate from table positions
+	// calculate the total salary and insert into table emp_salary
+	public void InsertEmployeeSalaryFromAttandance(String empid) {
+		String Date, Emp_id, emp_fullname, positionName;
+		double hourly_rate, OT_rate, total_hour, start_minute, end_minute, TotalPay;
+
+		try {
+			ResultSet rs = curs.executeQuery(
+					"SELECT emp_id,attendance_date " + "FROM Attendance" +
+							" WHERE emp_id =\"" + empid + "\" AND attendance_date = CURRENT_DATE"
+			);
+			while (rs.next()) {
 //
-//	public Boolean existsDepartment(String dep_name) {
-//		try {
-//			return curs.executeQuery(
-//					"SELECT * FROM departments WHERE dep_name=\"" + dep_name + "\""
-//				).next();
-//		} catch (SQLException e) {
-//			System.err.println(e.getMessage());
-//		}
-//		return false;
-//	}
-//
-//	public void newDepartment(String dep_name, int basic_salary, int da_percent, int hra_percent, int pf_percent) {
-//		int da = (da_percent / 100) * basic_salary;
-//		int hra = (hra_percent / 100) * basic_salary;
-//		int pf = (pf_percent / 100) * basic_salary;
-//		int gross_salary = basic_salary + da + hra + pf;
-//		int epf = pf / 2;
-//		int lic = epf / 2;
-//		int deductions = epf + lic;
-//		int net_salary = gross_salary - deductions;
-//
-//		try {
-//			curs.executeUpdate(
-//					"INSERT INTO departments VALUES(" +
-//							"null," +
-//							"\"" + dep_name + "\" ," +
-//							Integer.toString(basic_salary) + "," +
-//							Integer.toString(da) + "," +
-//							Integer.toString(hra) + "," +
-//							Integer.toString(pf) + "," +
-//							Integer.toString(gross_salary) + "," +
-//							Integer.toString(epf) + "," +
-//							Integer.toString(lic) + "," +
-//							Integer.toString(deductions) + "," +
-//							Integer.toString(net_salary) +
-//					")"
-//				);
-//		} catch (SQLException e) {
-//			System.err.println(e.getMessage());
-//		}
-//	}
-//
-//	public void deleteDepartment(String dep_name) {
-//		try {
-//			curs.executeUpdate(
-//					"DELETE FROM departments WHERE dep_name=\"" + dep_name + "\""
-//				);
-//			curs.executeUpdate(
-//					"DELETE FROM employees WHERE department=\"" + dep_name + "\""
-//				);
-//		} catch (SQLException e) {
-//			System.err.println(e.getMessage());
-//		}
-//	}
-//
-//	public void updateDepartment(String dep_name, int basic_salary, int da, int hra, int pf) {
-//		deleteDepartment(dep_name);
-//		newDepartment(dep_name, basic_salary, da, hra, pf);
-//	}
-//
-//	public ArrayList<String> getListOfDepartments() {
-//		ArrayList<String> lst = new ArrayList<String>();
-//
-//		try {
-//			ResultSet rs = curs.executeQuery("SELECT dep_name FROM departments");
-//
-//			while (rs.next()) {
-//				lst.add(rs.getString("dep_name"));
-//			}
-//		} catch (SQLException e) {
-//			System.err.println(e.getMessage());
-//		}
-//
-//		return lst;
-//	}
-//
-//	public int getSalary(String dep_name) {
-//		try {
-//			ResultSet rs = curs.executeQuery("SELECT net_salary FROM departments WHERE dep_name=\"" + dep_name + "\"");
-//
-//			if (rs.next())
-//				return rs.getInt("net_salary");
-//		} catch (SQLException e) {
-//			System.err.println(e.getMessage());
-//		}
-//		return 0;
-//	}
-//
-//	public Boolean existsEmployeeID(int id) {
-//		try {
-//			return curs.executeQuery(
-//					"SELECT * FROM employees WHERE id=" + Integer.toString(id)
-//				).next();
-//		} catch (SQLException e) {
-//			System.err.println(e.getMessage());
-//		}
-//		return false;
-//	}
-//
-//	public void createEmployee(String fn, String ln, String email, String department) {
-//		try {
-//			curs.executeUpdate("INSERT INTO employees VALUES(" +
-//					"null," +
-//					"\"" + fn + "\"," +
-//					"\"" + ln + "\"," +
-//					"\"" + email + "\"," +
-//					"\"" + department + "\"" +
-//				")");
-//		} catch (SQLException e) {
-//			System.err.println(e.getMessage());
-//		}
-//	}
-//
-//	public void deleteEmployee(int id) {
-//		try {
-//			curs.executeUpdate(
-//					"DELETE FROM employees WHERE id=" + Integer.toString(id)
-//				);
-//		} catch (SQLException e) {
-//			System.err.println(e.getMessage());
-//		}
-//	}
-//
-//	public void updateEmployee(int id, String fn, String ln, String email, String department) {
-//		try {
-//			curs.executeUpdate(
-//					"UPDATE employees SET " +
-//					"first_name=\"" + fn + "\"," +
-//					"last_name=\"" + ln + "\"," +
-//					"email=\"" + email + "\"," +
-//					"department=\"" + department + "\" " +
-//					"WHERE id=" + Integer.toString(id)
-//				);
-//		} catch (SQLException e) {
-//			System.err.println(e.getMessage());
-//		}
-//	}
-//
-//	public Object[][] getEmployees() {
-//		ArrayList<Object[]> employees = new ArrayList<Object[]>();
-//		ResultSet rs;
-//
-//		try {
-//			rs = curs.executeQuery(
-//					"SELECT * FROM employees"
-//				);
-//
-//			while (rs.next()) {
-//				Object[] temp = {
-//					rs.getInt("id"),
-//					rs.getString("first_name"),
-//					rs.getString("last_name"),
-//					rs.getString("email"),
-//					rs.getString("department"),
-//					getSalary(rs.getString("department"))
-//				};
-//
-//				employees.add(temp);
-//			}
-//		} catch (SQLException e) {
-//			System.err.println(e.getMessage());
-//		}
-//
-//		return employees.toArray(new Object[employees.size()][]);
-//	}
+				Emp_id = rs.getString("emp_id");
+				Date = rs.getString("attendance_date");
+				emp_fullname = getEmployeeName(rs.getString("emp_id"));
+				positionName = getPosition(Emp_id);
+				hourly_rate = getHourlyRate(positionName);
+				OT_rate = getOvertimeRate(positionName);
+				total_hour = getTotalHours(Emp_id);
+				if (total_hour > 8) {
+					TotalPay = ((8 * hourly_rate) + ((total_hour - 8) * OT_rate));
+				} else {
+					TotalPay = (total_hour * hourly_rate);
+				}
+
+				InsertEmpSalary(Date, Emp_id, emp_fullname, TotalPay, total_hour);
+			}
+
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	// get the total hours worked from table attendance
+	public double getTotalHours(String EmID) {
+		ResultSet rs;
+		try {
+			rs = curs.executeQuery(
+					"SELECT strftime('%H',clock_in_time) AS CIHours, strftime('%M',clock_in_time) AS CIMinutes," +
+							"strftime('%H',clock_out_time) AS COHours, strftime('%M',clock_out_time) AS COMinutes FROM Attendance " +
+							"WHERE emp_id=\"" + EmID + "\" AND attendance_date = CURRENT_DATE"
+			);
+			if (rs.next()) {
+				double CIH = (rs.getInt("CIHours") * 60); // get clockInTime Hours convert to minutes
+				double COH = (rs.getInt("COHours") * 60); // get clockOutTime Hours convert to minutes
+				double CIM = rs.getDouble("CIMinutes"); // get ClockInTime minutes
+				double COM = rs.getDouble("COMinutes");// get ClockOutTime minutes
+				double difference = ((COM + COH) - (CIM + CIH)); // calculate the difference between clock in and clock out in minutes
+				double DiffHours = (difference / 60); // convert from minutes to hours
+				return DiffHours;
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		return 0;
+	}
+
+	//check if position exist
+	public Boolean existsPosition(String pos_name) {
+		try {
+			return curs.executeQuery(
+					"SELECT * FROM Position WHERE pos_name=\"" + pos_name + "\""
+			).next();
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		return false;
+	}
+
+	//delete position and employee with that position
+	public void deletePosition(String pos_name) {
+		try {
+			curs.executeUpdate(
+					"DELETE FROM Position WHERE pos_name=\"" + pos_name + "\""
+			);
+
+			ResultSet rs = curs.executeQuery(
+					"SELECT id FROM employees WHERE pos_name =\"" + pos_name + "\""
+			);
+
+			deleteEmployee(rs.getString("id"));
+
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	// saved and return array for all the positions name
+	public ArrayList<String> getListOfPositionName() {
+		ArrayList<String> Positions = new ArrayList<String>();
+
+		try {
+			ResultSet rs = curs.executeQuery("SELECT pos_name FROM Position");
+
+			while (rs.next()) {
+				Positions.add(rs.getString("pos_name"));
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+		return Positions;
+	}
+
+	// Retrieve all the data from position table and store it into Object position , then return array of object position
+	// to be used in Jtable in Jframe
+	public Object[][] getPositions() {
+		ArrayList<Object[]> Positions = new ArrayList<Object[]>();
+
+		try {
+			ResultSet rs = curs.executeQuery("SELECT * FROM Position");
+
+			while (rs.next()) {
+
+				Object[] temp = {
+						rs.getInt("id"),
+						rs.getString("pos_name"),
+						rs.getInt("hourly_rate"),
+						rs.getInt("overtime_rate")
+
+
+				};
+
+				Positions.add(temp);
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+		return Positions.toArray(new Object[Positions.size()][]);
+	}
+
+	//Insert new employee data into table employee
+	public void createEmployee(String Empid, String fn, String ln, String email, String position) {
+		try {
+			curs.executeUpdate("INSERT INTO employees VALUES(" +
+					"\"" + Empid + "\"," +
+					"\"" + fn + "\"," +
+					"\"" + ln + "\"," +
+					"\"" + email + "\"," +
+					"\"" + position + "\"" +
+					")");
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	//verify if employee ID exist
+	public Boolean existsEmployeeID(String Empid) {
+		try {
+			return curs.executeQuery(
+					"SELECT * FROM employees WHERE id=\"" + Empid + "\""
+			).next();
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		return false;
+	}
+
+
+	//store all employee ID into array and return it
+	public ArrayList<String> getEmployeeID() {
+		ArrayList<String> EmployeeID = new ArrayList<String>();
+
+		try {
+			ResultSet rs = curs.executeQuery("SELECT id FROM employees");
+
+			while (rs.next()) {
+				EmployeeID.add(rs.getString("id"));
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+		return EmployeeID;
+	}
+
+	//delete employee data that match the input employee id as well as employee data in table attendance and
+	//salary with same ID
+	public void deleteEmployee(String Empid) {
+		try {
+			curs.executeUpdate(
+					"DELETE FROM employees WHERE id=\"" + Empid + "\""
+			);
+			curs.executeUpdate(
+					"DELETE FROM Attendance WHERE Emp_id=\"" + Empid + "\""
+			);
+			curs.executeUpdate(
+					"DELETE FROM emp_salary WHERE Emp_id=\"" + Empid + "\""
+			);
+
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	//update employee information
+	public void updateEmployee(String Empid, String fn, String ln, String email, String position) {
+		try {
+			curs.executeUpdate(
+					"UPDATE employees SET " +
+							"first_name=\"" + fn + "\"," +
+							"last_name=\"" + ln + "\"," +
+							"email=\"" + email + "\"," +
+							"pos_name=\"" + position + "\" " +
+							"WHERE id=\"" + Empid + "\""
+			);
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	// get and return full name of employee from employee ID
+	public String getEmployeeName(String empid) {
+		String FullName;
+		try {
+			ResultSet rs = curs.executeQuery("SELECT  first_name , last_name  FROM employees WHERE id=\""
+					+ empid + "\"");
+
+			if (rs.next()) {
+				return (FullName = (rs.getString("first_name") + " " + rs.getString("last_name")));
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+		return "";
+	}
+
+	//get all employee data and store into employee object . Store and return employee Object in an array of object
+	//to be used in Jtable for Jframe
+	public Object[][] getEmployees() {
+
+		ArrayList<Object[]> employees = new ArrayList<Object[]>();
+		ResultSet rs;
+
+		try {
+			rs = curs.executeQuery(
+					"SELECT * FROM employees"
+			);
+
+			while (rs.next()) {
+				Object[] temp = {
+						rs.getString("id"),
+						rs.getString("first_name"),
+						rs.getString("last_name"),
+						rs.getString("email"),
+						rs.getString("pos_name")
+				};
+
+				employees.add(temp);
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+		return employees.toArray(new Object[employees.size()][]);
+	}
+
+	// get the employee position from employee ID
+	public String getPosition(String EmpID) {
+		try {
+			ResultSet rs = curs.executeQuery("SELECT pos_name FROM employees WHERE id =\"" + EmpID + "\"");
+
+			if (rs.next())
+				return rs.getString("pos_name");
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+		return "";
+	}
+
+	//get the hourly rate from position with matching position name
+	public int getHourlyRate(String pos_name) {
+		try {
+			ResultSet rs = curs.executeQuery("SELECT hourly_rate FROM Position WHERE pos_name =\"" + pos_name + "\"");
+
+			if (rs.next())
+				return rs.getInt("hourly_rate");
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		return 0;
+	}
+
+	//get the hourly rate from position with matching position name
+	public int getOvertimeRate(String pos_name) {
+		try {
+			ResultSet rs = curs.executeQuery("SELECT overtime_rate FROM Position WHERE pos_name =\"" + pos_name + "\"");
+
+			if (rs.next())
+				return rs.getInt("overtime_rate");
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		return 0;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
